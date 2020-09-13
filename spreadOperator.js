e82a0ab2f29caf24728da0e93a1f3de0d01144f0
muhammad shahid <shahidjaber90@gmail.com>
2020-09-14 03:01:54 +0500

    added My Task 4

---
+++ spreadOperator.js
@@ -0,0 +1,18 @@
+
+let myObj = {
+    firstName: 'Muhammad',
+    lastName: 'Shahid',
+    address: {
+        address1: 'Karachi',
+        address2: 'Lahore'
+    }
+}
+
+let mySecondObj = {
+    ...myObj,
+
+    address:   {address1:'Karachi', address2: 'Islamabad'}
+};
+
+console.log('My First Obj =====> ' , myObj );
+console.log('My Second Obj ====>',  mySecondObj);
